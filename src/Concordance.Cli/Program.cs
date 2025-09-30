2a6e9547eb20c135a393485aa3c1585c643625fe
Matt Janda <matt.janda.kingston@gmail.com>
2025-09-30 20:39:28 +0530

    Dispose logger in cli app

--- src/Concordance.Cli/Program.cs
+++ src/Concordance.Cli/Program.cs
@@ -6,8 +6,8 @@ using Serilog.Sinks.SystemConsole.Themes;
 
 try
 {
-    using var serviceProvider = new ServiceCollection()
-        .AddLogging(builder => builder.AddSerilog(new LoggerConfiguration()            
+    using (var serviceProvider = new ServiceCollection()
+        .AddLogging(builder => builder.AddSerilog(new LoggerConfiguration()
             .Enrich.WithStopwatch(string.Empty)
             .Enrich.WithIoMetrics()
             .Enrich.WithMemoryMetrics()
@@ -15,39 +15,40 @@ try
                 theme: AnsiConsoleTheme.Code,
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} ({Stopwatch}) [{Level:u4}] {Message} | Read {IoReadBytes:N0} | Memory {WorkingSetBytes:N0}{NewLine}{Exception}")
             .WriteTo.File($"logs/concordance-{DateTime.Now.ToString("yyyMMdd-HHmmss")}.log",
-                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u4}] {Message}{NewLine} | Elapsed {Stopwatch} | Read {IoReadBytes:N0} | Memory {WorkingSetBytes:N0}{Exception}")
-            .CreateLogger()))
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u4}] {Message} | Elapsed {Stopwatch} | Read {IoReadBytes:N0} | Memory {WorkingSetBytes:N0}{NewLine}{Exception}")
+            .CreateLogger(), dispose: true))
         .AddTransient<Main>()
-        .BuildServiceProvider();
-
-    try
+        .BuildServiceProvider())
     {
-        using var cancellationTokenSource = new CancellationTokenSource();
 
-        Console.CancelKeyPress += (sender, eventArgs) =>
+        try
         {
-            if (!cancellationTokenSource.IsCancellationRequested) {
-                serviceProvider.GetRequiredService<ILogger<Program>>()
-                    .LogWarning("User break (Ctrl+C) detected. Shutting down gracefully...");
-            
-                cancellationTokenSource.Cancel();
-                eventArgs.Cancel = true; 
-            }
-        };
+            using var cancellationTokenSource = new CancellationTokenSource();
 
-        await serviceProvider
-            .GetRequiredService<Main>()
-            .RunAsync(cancellationTokenSource.Token);
-    }
-    catch (Exception ex)
-    {
-        serviceProvider.GetService<ILogger<Program>>()
-            .LogCritical(ex, "Program failed.");
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                if (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    serviceProvider.GetRequiredService<ILogger<Program>>()
+                        .LogWarning("User break (Ctrl+C) detected. Shutting down gracefully...");
+
+                    cancellationTokenSource.Cancel();
+                    eventArgs.Cancel = true;
+                }
+            };
+
+            await serviceProvider
+                .GetRequiredService<Main>()
+                .RunAsync(cancellationTokenSource.Token);
+        }
+        catch (Exception ex)
+        {
+            serviceProvider.GetService<ILogger<Program>>()
+                .LogCritical(ex, "Program failed.");
+        }
     }
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
-
